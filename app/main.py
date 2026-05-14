2e042957587d88029b569ed09e3b4d218180659a
P4rd0D3v <carlos.pardo.b78@gmail.com>
2026-05-13 23:54:55 -0400

    allow origins

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.router import api_router
 from app.config import settings
 from app.db.session import engine, Base
@@ -7,5 +8,13 @@ import app.models.producto  # noqa: ensure model registered before create_all
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://frontend-prueba-six.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api/v1")
